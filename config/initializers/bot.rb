d5e018e1413bf6225419b3387c940bff5bf9003e
Isaac Priestley <progressions@gmail.com>
2022-12-23 08:54:15 -0500

    Try using edge discordrb

--- config/initializers/bot.rb
+++ config/initializers/bot.rb
@@ -12,3 +12,17 @@ Dir["#{Rails.root}/app/commands/*.rb"].each { |file| require file }
 
 Bot.run(true)
 puts "Invite URL: #{Bot.invite_url}"
+
+bot = Discordrb::Bot.new(
+  token: Rails.application.credentials.dig(:discord, :token),
+  client_id: Rails.application.credentials.dig(:discord, :client_id),
+  intents: :all
+)
+bot.register_application_command(:hello, 'Say hello', server_id: 833455198152032256) do |cmd|
+  cmd.string('message', 'Say a message')
+end
+
+bot.application_command(:hello) do |event|
+  puts event.inspect
+  event.respond(content: "Thanks!")
+end
