82e2a2604706ba3d4fd49f145ffe09b175a614f2
Isaac Priestley <progressions@gmail.com>
2022-12-23 13:44:46 -0500

    Format shots

--- app/services/fight_poster.rb
+++ app/services/fight_poster.rb
@@ -9,6 +9,10 @@ module FightPoster
       Bot.send_message(ChannelID, message(fight))
     end
 
+    def post_shots(fight)
+      Bot.send_message(ChannelID, shots(fight))
+    end
+
     def message(fight)
       message = <<-TEXT
         A new fight has been created: #{fight.name}
@@ -20,6 +24,35 @@ TEXT
       opts = Rails.application.config.action_mailer.default_url_options
       port = opts[:port] ? ":#{opts[:port]}" : nil
       "#{opts[:protocol]}://#{opts[:host]}#{port}/fights/#{fight.id}"
+    end
+
+    def shots(fight)
+      message = []
+
+      message << "FIGHT"
+      message << ""
+      message << fight.name
+      message << "(sequence 1)"
+      message << "================="
+
+      fight.shot_order.each do |shot, characters|
+        shot_msg = []
+        shot_msg << "Shot #{shot}"
+        shot_msg << characters.map do |character|
+          char_msg = []
+          char_msg << "**#{character.name}**"
+          if character.defense
+            char_msg << "(D#{character.defense})"
+          end
+          if character.impairments.to_i > 0
+            char_msg << "(-#{character.impairments})"
+          end
+          char_msg.join(" ")
+        end.join(", ")
+        message << shot_msg.join(" ")
+      end
+
+      message.join("\n")
     end
 
   end
