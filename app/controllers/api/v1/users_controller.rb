ca3a3a0d255d96f4ac9595a6ddca93bae0eef481
Isaac Priestley <progressions@gmail.com>
2022-12-30 20:45:35 -0500

    Permit admin

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -29,6 +29,6 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :avatar_url, :first_name, :last_name)
+    params.require(:user).permit(:email, :avatar_url, :first_name, :last_name, :admin)
   end
 end
