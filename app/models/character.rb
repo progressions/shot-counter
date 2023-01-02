156547acb6e8f329f379fc212eb0cb5e772bbc58
Isaac Priestley <progressions@gmail.com>
2023-01-02 10:03:52 -0500

    Ensure action values

--- app/models/character.rb
+++ app/models/character.rb
@@ -7,10 +7,10 @@ class Character < ApplicationRecord
     "Scroungetech" => nil,
     "Genome" => nil,
     "Defense" => 13,
-    "Toughness" => 6,
-    "Speed" => 6,
-    "Fortune" => 6,
-    "Max Fortune" => 6,
+    "Toughness" => nil,
+    "Speed" => nil,
+    "Fortune" => nil,
+    "Max Fortune" => nil,
     "FortuneType" => "Fortune",
     "MainAttack" => "Guns",
     "SecondaryAttack" => "Martial Arts",
@@ -20,16 +20,24 @@ class Character < ApplicationRecord
   belongs_to :fight
   belongs_to :user, optional: true
 
-  # before_save :add_default_action_values
-
-  def add_default_action_values
-    self.action_values ||= {}
-    self.action_values = self.action_values.merge(DEFAULT_ACTION_VALUES)
-  end
+  after_initialize :add_default_action_values
+  before_save :ensure_action_values
 
   def act!(shots=DEFAULT_SHOT_COUNT)
     self.current_shot ||= 0
     self.current_shot -= shots.to_i
     save!
+  end
+
+  private
+
+  def ensure_action_values
+    self.action_values ||= {}
+    self.action_values = DEFAULT_ACTION_VALUES.merge(self.action_values)
+  end
+
+  def add_default_action_values
+    self.action_values ||= {}
+    self.action_values = self.action_values.merge(DEFAULT_ACTION_VALUES)
   end
 end
