71da9f63dcc62c47cf3f052ad8c70d4267a6244d
Isaac Priestley <progressions@gmail.com>
2023-01-04 10:51:27 -0500

    Fix migration

--- db/migrate/20230104153708_create_fight_characters.rb
+++ db/migrate/20230104153708_create_fight_characters.rb
@@ -1,7 +1,7 @@
 class CreateFightCharacters < ActiveRecord::Migration[7.0]
   def change
     create_table :fight_characters, id: :uuid do |t|
-      t.references :user, type: :uuid, null: false, foreign_key: true
+      t.references :character, type: :uuid, null: false, foreign_key: true
       t.references :fight, type: :uuid, null: false, foreign_key: true
 
       t.timestamps
