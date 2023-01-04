4d3633db8e2033020b7b945d99dfa1d881a47118
Isaac Priestley <progressions@gmail.com>
2023-01-04 10:37:44 -0500

    Create FightCharacters join table

---
+++ db/migrate/20230104153708_create_fight_characters.rb
@@ -0,0 +1,10 @@
+class CreateFightCharacters < ActiveRecord::Migration[7.0]
+  def change
+    create_table :fight_characters, id: :uuid do |t|
+      t.references :user, type: :uuid, null: false, foreign_key: true
+      t.references :fight, type: :uuid, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_04_152852) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_04_153708) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -59,6 +59,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_04_152852) do
     t.index ["user_id"], name: "index_characters_on_user_id"
   end
 
+  create_table "fight_characters", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "user_id", null: false
+    t.uuid "fight_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["fight_id"], name: "index_fight_characters_on_fight_id"
+    t.index ["user_id"], name: "index_fight_characters_on_user_id"
+  end
+
   create_table "fights", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -88,4 +97,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_04_152852) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "characters", "fights"
   add_foreign_key "characters", "users"
+  add_foreign_key "fight_characters", "fights"
+  add_foreign_key "fight_characters", "users"
 end
