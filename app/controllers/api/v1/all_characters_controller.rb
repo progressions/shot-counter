1462521d12f95a08904c6302623ae95af06cfe42
Isaac Priestley <progressions@gmail.com>
2023-01-05 18:48:47 -0500

    Manipulate characters independent of a fight

--- app/controllers/api/v1/all_characters_controller.rb
+++ app/controllers/api/v1/all_characters_controller.rb
@@ -1,13 +1,46 @@
 class Api::V1::AllCharactersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_scoped_characters
+  before_action :set_character, only: [:update, :destroy, :show]
 
   def index
-    @characters = @scoped_characters.all
+    @characters = @scoped_characters.includes(:user).all
     render json: @characters
   end
 
+  def create
+    @character = Character.create!(character_params)
+    @character.user = current_user
+
+    if @fight_character.save
+      render json: @character
+    else
+      render status: 400
+    end
+  end
+
+  def show
+    render json: @character
+  end
+
+  def update
+    if @character.update(character_params)
+      render json: @character
+    else
+      render @character.errors, status: 400
+    end
+  end
+
+  def destroy
+    @character.destroy!
+    render :ok
+  end
+
   private
+
+  def set_character
+    @character = @scoped_characters.find(params[:id])
+  end
 
   def set_scoped_characters
     if current_user.gamemaster?
@@ -15,6 +48,10 @@ class Api::V1::AllCharactersController < ApplicationController
     else
       @scoped_characters = current_user.characters
     end
+  end
+
+  def character_params
+    params.require(:character).permit(:name, :defense, :impairments, :color, :user_id, action_values: Character::DEFAULT_ACTION_VALUES.keys)
   end
 
 end
