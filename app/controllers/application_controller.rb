66416e63ae716808d9ab4488156cd013aa33e79a
Isaac Priestley <progressions@gmail.com>
2023-01-15 11:01:22 -0500

    Get campaign from query param

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  before_action :set_campaign
+
+  private
+
+  def set_campaign
+    @campaign = Campaign.find_by(id: params[:campaign_id])
+  end
+
+  def campaign
+    @campaign
+  end
 end
