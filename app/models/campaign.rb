3ef363519c8ddf34cdc7904264ccd817888520fd
Isaac Priestley <progressions@gmail.com>
2023-01-15 11:11:28 -0500

    JSON for campaign

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,4 +2,13 @@ class Campaign < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true
+
+  def as_json(args={})
+    {
+      id: id,
+      title: title,
+      description: description,
+      user: user
+    }
+  end
 end
