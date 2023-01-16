3b4ae313e902cbdfb98ef1a031db6736ff91205c
Isaac Priestley <progressions@gmail.com>
2023-01-16 09:39:55 -0500

    User has vehicles

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
 
   has_many :campaigns
   has_many :characters
+  has_many :vehicles
   has_many :campaign_memberships
   has_many :player_campaigns, through: :campaign_memberships, source: "campaign"
 
