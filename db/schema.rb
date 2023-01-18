0226e70c8e89266166f4e86194cac96ad80f8789
Isaac Priestley <progressions@gmail.com>
2023-01-18 10:47:36 -0500

    Add archived attribute to fight

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_18_143234) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_18_154648) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -112,6 +112,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_18_143234) do
     t.integer "sequence", default: 0, null: false
     t.uuid "campaign_id"
     t.boolean "active", default: true, null: false
+    t.boolean "archived", default: true, null: false
     t.index ["campaign_id"], name: "index_fights_on_campaign_id"
   end
 
