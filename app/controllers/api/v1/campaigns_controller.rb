71959bc62ef252b2c673f5187f6eb820f70d8139
Isaac Priestley <progressions@gmail.com>
2023-01-19 13:12:11 -0500

    Update campaigns controller

--- app/controllers/api/v1/campaigns_controller.rb
+++ app/controllers/api/v1/campaigns_controller.rb
@@ -1,15 +1,19 @@
 class Api::V1::CampaignsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_scoped_campaigns
+  before_action :set_campaign, only: [:show, :set]
 
   def index
-    @campaigns = @scoped_campaigns.order(:title)
+    @campaigns = { gamemaster: current_user.campaigns, player: current_user.player_campaigns }
 
     render json: @campaigns
   end
 
   def create
-    @campaign = @scoped_campaigns.new(campaign_params)
+    if !current_user.gamemaster
+      render status: 403 and return
+    end
+
+    @campaign = current_user.campaigns.new(campaign_params)
 
     if @campaign.save
       render json: @campaign
@@ -19,21 +23,23 @@ class Api::V1::CampaignsController < ApplicationController
   end
 
   def show
-    if params[:id] == "current"
-      render json: current_campaign
+    if @campaign
+      render json: @campaign
     else
-      @campaign = @scoped_campaigns.find_by(id: params[:id])
-
-      if @campaign
-        render json: @campaign
-      else
-        render status: 404
-      end
+      render status: 404
     end
   end
 
   def update
-    @campaign = @scoped_campaigns.find_by(id: params[:id])
+    if !current_user.gamemaster
+      render status: 403 and return
+    end
+
+    @campaign = (current_user.gamemaster && current_user.campaigns.find_by(id: params[:id]))
+
+    if @campaign.nil?
+      render status: 404 and return
+    end
 
     if @campaign.update(campaign_params)
       render json: @campaign
@@ -43,15 +49,22 @@ class Api::V1::CampaignsController < ApplicationController
   end
 
   def destroy
-    @campaign = @scoped_campaigns.find_by(id: params[:id])
-    @campaign.destroy!
+    if !current_user.gamemaster
+      render status: 403 and return
+    end
 
-    render :ok
+    # Only a gamemaster can destroy a campaign
+    @campaign = (current_user.gamemaster && current_user.campaigns.find_by(id: params[:id]))
+    if @campaign
+      @campaign.destroy!
+
+      render :ok and return
+    else
+      render status: 404
+    end
   end
 
   def set
-    @campaign = @scoped_campaigns.find_by(id: params[:id])
-
     user_info = {
       "campaign_id" => @campaign&.id
     }
@@ -66,11 +79,11 @@ class Api::V1::CampaignsController < ApplicationController
 
   private
 
-  def set_scoped_campaigns
-    if current_user.gamemaster?
-      @scoped_campaigns = current_user.campaigns
+  def set_campaign
+    if params[:id] == "current"
+      @campaign = current_campaign
     else
-      @scoped_campaigns = current_user.player_campaigns
+      @campaign = (current_user.gamemaster && current_user.campaigns.find_by(id: params[:id])) || current_user.player_campaigns.find_by(id: params[:id])
     end
   end
 
--- spec/requests/api/v1/campaigns_spec.rb
+++ spec/requests/api/v1/campaigns_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "Api::V1::Campaigns", type: :request do
   before(:each) do
     @gamemaster = User.create!(email: "email@example.com", gamemaster: true)
+    @other_gamemaster = User.create!(email: "other@example.com", gamemaster: true)
     @headers = Devise::JWT::TestHelpers.auth_headers({}, @gamemaster)
   end
 
@@ -14,7 +15,28 @@ RSpec.describe "Api::V1::Campaigns", type: :request do
       get "/api/v1/campaigns", headers: @headers
       expect(response).to have_http_status(:success)
       campaigns = JSON.parse(response.body)
-      expect(campaigns.map { |c| c["title"] }).to eq(["Action Movie", "Adventure"])
+      expect(campaigns["gamemaster"].map { |c| c["title"] }).to eq(["Action Movie", "Adventure"])
+      expect(campaigns["player"]).to be_empty
+    end
+
+    it "returns all my campaigns, both ones I run and ones I play in" do
+      @third_gamemaster = User.create!(email: "third@example.com", gamemaster: true)
+      @modern = @third_gamemaster.campaigns.create!(title: "Modern")
+
+      @action_movie = @gamemaster.campaigns.create!(title: "Action Movie")
+      @adventure = @gamemaster.campaigns.create!(title: "Adventure")
+
+      @scifi = @other_gamemaster.campaigns.create!(title: "Scifi")
+      @fantasy = @other_gamemaster.campaigns.create!(title: "Fantasy")
+
+      @scifi.players << @gamemaster
+      @fantasy.players << @gamemaster
+
+      get "/api/v1/campaigns", headers: @headers
+      expect(response).to have_http_status(:success)
+      campaigns = JSON.parse(response.body)
+      expect(campaigns["gamemaster"].map { |c| c["title"] }).to eq(["Action Movie", "Adventure"])
+      expect(campaigns["player"].map { |c| c["title"] }).to eq(["Scifi", "Fantasy"])
     end
   end
 
@@ -30,6 +52,19 @@ RSpec.describe "Api::V1::Campaigns", type: :request do
       expect(body["title"]).to eq("Hard to Kill")
     end
 
+    it "returns error if you're unauthorized" do
+      @player = User.create!(email: "player@email.com", gamemaster: false)
+      @headers = Devise::JWT::TestHelpers.auth_headers({}, @player)
+
+      post "/api/v1/campaigns", headers: @headers, params: {
+        campaign: {
+          title: "Hard to Make"
+        }
+      }
+      expect(response).to have_http_status(:forbidden)
+      expect(Campaign.count).to eq(0)
+    end
+
     it "returns errors" do
       post "/api/v1/campaigns", headers: @headers, params: {
         campaign: {
@@ -43,7 +78,7 @@ RSpec.describe "Api::V1::Campaigns", type: :request do
   end
 
   describe "GET /campaigns/:id" do
-    it "fetches campaign" do
+    it "fetches campaign you created" do
       @campaign = @gamemaster.campaigns.create!(title: "Action Movie")
 
       @alice = User.create!(email: "alice@example.com")
@@ -57,6 +92,31 @@ RSpec.describe "Api::V1::Campaigns", type: :request do
       body = JSON.parse(response.body)
       expect(body["title"]).to eq("Action Movie")
       expect(body["players"].map { |u| u["email"] }).to eq(["alice@example.com", "marcie@example.com"])
+    end
+
+    it "fetches campaign you play in" do
+      @campaign = @gamemaster.campaigns.create!(title: "Action Movie")
+
+      @alice = User.create!(email: "alice@example.com")
+      @campaign.players << @alice
+
+      @headers = Devise::JWT::TestHelpers.auth_headers({}, @alice)
+
+      get "/api/v1/campaigns/#{@campaign.id}", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["title"]).to eq("Action Movie")
+    end
+
+    it "fetches the current campaign" do
+      @campaign = @gamemaster.campaigns.create!(title: "Action Movie")
+
+      post "/api/v1/campaigns/current", params: { id: @campaign.id }, headers: @headers
+      get "/api/v1/campaigns/#{@campaign.id}", headers: @headers
+
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["title"]).to eq("Action Movie")
     end
 
     it "returns 404" do
@@ -77,6 +137,22 @@ RSpec.describe "Api::V1::Campaigns", type: :request do
       body = JSON.parse(response.body)
       expect(body["title"]).to eq("Hard to Kill")
     end
+
+    it "can't update a campaign you're just a player in" do
+      @campaign = @gamemaster.campaigns.create!(title: "Action Movie")
+      @alice = User.create!(email: "alice@example.com")
+      @campaign.players << @alice
+
+      @headers = Devise::JWT::TestHelpers.auth_headers({}, @alice)
+
+      patch "/api/v1/campaigns/#{@campaign.id}", headers: @headers, params: {
+        campaign: {
+          title: "Hard to Change"
+        }
+      }
+      expect(response).to have_http_status(:forbidden)
+      expect(@campaign.reload.title).to eq("Action Movie")
+    end
   end
 
   describe "DESTROY /campaigns/:id" do
@@ -85,6 +161,30 @@ RSpec.describe "Api::V1::Campaigns", type: :request do
       delete "/api/v1/campaigns/#{@campaign.id}", headers: @headers
       expect(response).to have_http_status(:success)
       expect(Campaign.find_by(id: @campaign.id)).to be_nil
+    end
+
+    it "can't destroy a campaign you're just a player in" do
+      @campaign = @gamemaster.campaigns.create!(title: "Action Movie")
+      @alice = User.create!(email: "alice@example.com")
+      @campaign.players << @alice
+
+      @headers = Devise::JWT::TestHelpers.auth_headers({}, @alice)
+
+      delete "/api/v1/campaigns/#{@campaign.id}", headers: @headers
+      expect(response).to have_http_status(:forbidden)
+      expect(@campaign.reload).to be_present
+    end
+
+    it "even for a gamemaster, can't destroy a campaign you're a player in" do
+      @campaign = @gamemaster.campaigns.create!(title: "Action Movie")
+      @gm_alice = User.create!(email: "alice@example.com", gamemaster: true)
+      @campaign.players << @gm_alice
+
+      @headers = Devise::JWT::TestHelpers.auth_headers({}, @gm_alice)
+
+      delete "/api/v1/campaigns/#{@campaign.id}", headers: @headers
+      expect(response).to have_http_status(:not_found)
+      expect(@campaign.reload).to be_present
     end
   end
 
@@ -115,6 +215,21 @@ RSpec.describe "Api::V1::Campaigns", type: :request do
 
       post "/api/v1/campaigns/current", params: { id: @campaign.id }, headers: @headers
       expect(response).to have_http_status(:unauthorized)
+    end
+
+    it "can set campaign you're a player in" do
+      @campaign = @gamemaster.campaigns.create!(title: "Adventure")
+      @alice = User.create!(email: "alice@example.com")
+      @campaign.players << @alice
+
+      @headers = Devise::JWT::TestHelpers.auth_headers({}, @alice)
+
+      post "/api/v1/campaigns/current", params: { id: @campaign.id }, headers: @headers
+      expect(response).to have_http_status(:success)
+
+      redis = Redis.new
+      user_info = JSON.parse(redis.get("user_#{@alice.id}"))
+      expect(user_info["campaign_id"]).to eq(@campaign.id)
     end
   end
 
