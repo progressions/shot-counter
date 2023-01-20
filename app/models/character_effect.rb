da5a4172a467079cfdb40259b07ca0abafa3fb89
Isaac Priestley <progressions@gmail.com>
2023-01-20 12:47:07 -0500

    Json for character effects

--- app/models/character_effect.rb
+++ app/models/character_effect.rb
@@ -1,4 +1,17 @@
 class CharacterEffect < ApplicationRecord
   belongs_to :character
   belongs_to :fight
+
+  def as_json(args={})
+    {
+      id: id,
+      title: title,
+      character: {
+        id: character.id
+      },
+      fight: {
+        id: fight.id
+      }
+    }
+  end
 end
