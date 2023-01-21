3fa1020d58b460af0694ba3b7ad84a965637ec62
Isaac Priestley <progressions@gmail.com>
2023-01-21 10:08:35 -0500

    Ensure character belongs to the same fight

--- app/models/fight_character.rb
+++ app/models/fight_character.rb
@@ -3,9 +3,22 @@ class FightCharacter < ApplicationRecord
   belongs_to :character, optional: true
   belongs_to :vehicle, optional: true
 
+  validate :ensure_campaign
+
   def act!(shot_cost: Fight::DEFAULT_SHOT_COUNT)
     self.shot ||= 0
     self.shot -= shot_cost.to_i
     save!
+  end
+
+  private
+
+  def ensure_campaign
+    if (self.character && self.character.campaign != self.fight.campaign)
+      errors.add(:character, "must belong to the same campaign as its fight")
+    end
+    if (self.vehicle && self.vehicle.campaign != self.fight.campaign)
+      errors.add(:vehicle, "must belong to the same campaign as its fight")
+    end
   end
 end
