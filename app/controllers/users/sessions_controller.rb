26a8e46e7fa02ee07b1d294f2e1dd4befe064b06
Isaac Priestley <progressions@gmail.com>
2023-01-21 20:28:03 -0500

    Include updates to controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,39 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
+  respond_to :json
+
+  private
+
+  def respond_with(resource, options={})
+    render json: {
+      code: 200,
+      message: 'User signed in successfully',
+      data: current_user,
+      payload: current_user.jwt_payload
+    }
+  end
+
+  def respond_to_on_destroy
+    jwt_payload = JWT.decode(
+      request.headers['Authorization'].split(' ')[1],
+      Rails.application.credentials.devise_jwt_secret_key!
+    ).first
+
+    current_user = User.find(jwt_payload['sub'])
+    if current_user
+      render json: {
+        status: 200,
+        message: 'Signed out successfully'
+      }
+    else
+      render json: {
+        status: 401,
+        message: 'User has no active session'
+      }, status: :unauthorized
+    end
+  end
+
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -25,3 +58,43 @@ class Users::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
 end
+=begin
+
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  respond_to :json
+
+  private
+
+  def respond_with(resource, options={})
+    render json: {
+      code: 200,
+      message: 'User signed in successfully',
+      data: current_user,
+      payload: current_user.jwt_payload
+    }
+  end
+
+  def respond_to_on_destroy
+    jwt_payload = JWT.decode(
+      request.headers['Authorization'].split(' ')[1],
+      Rails.application.credentials.devise_jwt_secret_key!
+    ).first
+
+    current_user = User.find(jwt_payload['sub'])
+    if current_user
+      render json: {
+        status: 200,
+        message: 'Signed out successfully'
+      }
+    else
+      render json: {
+        status: 401,
+        message: 'User has no active session'
+      }, status: :unauthorized
+    end
+  end
+end
+
+=end
