5d29be9ad81722a23310978310d551a5a9b81248
Isaac Priestley <progressions@gmail.com>
2023-01-24 11:57:24 -0500

    Start spec for all_characters

--- spec/requests/api/v1/all_characters_spec.rb
+++ spec/requests/api/v1/all_characters_spec.rb
@@ -1,7 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::AllCharacters", type: :request do
+  before(:each) do
+    @gamemaster = User.create!(email: "email@example.com", confirmed_at: Time.now, gamemaster: true)
+    @campaign = @gamemaster.campaigns.create!(title: "Adventure")
+    @fight = @campaign.fights.create!(name: "Big Brawl")
+    @brick = Character.create!(name: "Brick Manly", action_values: { "Type" => "PC" }, campaign_id: @campaign.id)
+    @boss = Character.create!(name: "Ugly Shing", action_values: { "Type" => "Boss" }, campaign_id: @campaign.id)
+    @headers = Devise::JWT::TestHelpers.auth_headers({}, @gamemaster)
+    set_current_campaign(@gamemaster, @campaign)
+  end
+
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets all characters" do
+      get "/api/v1/all_characters", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body.map { |c| c["name"] }).to eq(["Brick Manly", "Ugly Shing"])
+    end
+  end
+
+  def set_current_campaign(user, campaign)
+    redis = Redis.new
+    user_info = {
+      "campaign_id" => campaign&.id
+    }
+    redis.set("user_#{user.id}", user_info.to_json)
   end
 end
