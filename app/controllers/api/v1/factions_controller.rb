1ee74e10e35340ad89989298489f247ae34b0276
Isaac Priestley <progressions@gmail.com>
2023-02-05 08:45:43 -0500

    Don't send blank factions

--- app/controllers/api/v1/factions_controller.rb
+++ app/controllers/api/v1/factions_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::FactionsController < ApplicationController
   before_action :require_current_campaign
 
   def index
-    @factions = current_campaign.characters.pluck(Arel.sql("action_values -> 'Faction'")).uniq
+    @factions = current_campaign.characters.pluck(Arel.sql("action_values -> 'Faction'")).reject { |f| f.blank? }.uniq
 
     render json: @factions
   end
