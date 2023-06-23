0d99d7c27c8bed6e0e44dfd8166b75ab4ee3bf4c
Isaac Priestley <isaac.priestley@babylist.com>
2023-06-22 20:30:43 -0400

    Sort factions alphabetically

--- app/controllers/api/v1/factions_controller.rb
+++ app/controllers/api/v1/factions_controller.rb
@@ -3,7 +3,12 @@ class Api::V1::FactionsController < ApplicationController
   before_action :require_current_campaign
 
   def index
-    @factions = current_campaign.characters.pluck(Arel.sql("action_values -> 'Faction'")).reject { |f| f.blank? }.uniq
+    @factions = current_campaign
+      .characters
+      .pluck(Arel.sql("action_values -> 'Faction'"))
+      .reject { |f| f.blank? }
+      .uniq
+      .sort
 
     render json: @factions
   end
