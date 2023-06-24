a891d908dc0f131750c11e80680cff28b00fb320
Isaac Priestley <isaac.priestley@babylist.com>
2023-06-24 17:56:18 -0400

    line break

--- app/services/fight_poster.rb
+++ app/services/fight_poster.rb
@@ -47,6 +47,7 @@ TEXT
       fight.shot_order.each do |shot, characters|
         shot_msg = []
 
+        shot_msg << "\n"
         shot_msg << "```"
         shot_msg << "- Shot #{shot.to_i}"
         shot_msg << "```"
