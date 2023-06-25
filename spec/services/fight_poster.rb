757a8987786b01f3c64041b1707a475be9b03db6
Isaac Priestley <isaac.priestley@babylist.com>
2023-06-25 11:59:31 -0400

    Don't show stats for GMCs

--- spec/services/fight_poster.rb
+++ spec/services/fight_poster.rb
@@ -77,4 +77,64 @@ RSpec.describe FightPoster do
       expect(response).to eq(expected)
     end
   end
+
+  context "with multiple characters of different types" do
+    before(:each) do
+      # Uber-Boss
+      thunder_king = Character.create!(name: "Thunder King", action_values: {"Type" => "Uber-Boss", "Guns" => 18, "Defense" => 17, "Toughness" => 9, "Speed" => 8}, campaign_id: action_movie.id)
+      # Boss
+      shing = Character.create!(name: "Ugly Shing", action_values: {"Type" => "Boss", "Guns" => 15, "Defense" => 14, "Toughness" => 7, "Speed" => 7}, campaign_id: action_movie.id)
+      # Featured Foe
+      hitman = Character.create!(name: "Hitman", action_values: {"Type" => "Featured Foe", "Guns" => 15, "Defense" => 14, "Toughness" => 7, "Speed" => 7}, campaign_id: action_movie.id)
+      # Ally
+      jawbuster = Character.create!(name: "Jawbuster", action_values: {"Type" => "Ally", "Guns" => 15, "Defense" => 14, "Toughness" => 7, "Speed" => 7}, campaign_id: action_movie.id)
+      # Mook
+      mook = Character.create!(name: "Ninja", action_values: {"Type" => "Mook", "Guns" => 8, "Defense" => 13, "Toughness" => 7, "Speed" => 6}, campaign_id: action_movie.id)
+      # PC
+      brick = Character.create!(name: "Brick Manly", action_values: {"Type" => "PC", "Guns" => 15, "Defense" => 14, "Toughness" => 7, "Speed" => 7, "Fortune" => 7, "Max Fortune" => 7}, campaign_id: action_movie.id)
+      # PC
+      serena = Character.create!(name: "Serena", action_values: {"Type" => "PC", "MainAttack" => "Sorcery", "FortuneType" => "Magic", "Sorcery" => 14, "Defense" => 13, "Toughness" => 7, "Speed" => 6, "Fortune" => 5, "Max Fortune" => 7}, campaign_id: action_movie.id, impairments: 1)
+
+      fight.fight_characters.create!(character: mook, shot: 8)
+      fight.fight_characters.create!(character: jawbuster, shot: 10)
+      fight.fight_characters.create!(character: hitman, shot: 9)
+      fight.fight_characters.create!(character: shing, shot: 10)
+      fight.fight_characters.create!(character: brick, shot: 12)
+      fight.fight_characters.create!(character: serena, shot: 14)
+      fight.fight_characters.create!(character: thunder_king, shot: 12)
+    end
+
+    let(:expected) do
+      <<-TEXT
+# Museum Battle
+### Sequence 0
+## Shot 14
+- **Serena**
+ 0 Wounds (1 Impairment)
+ Sorcery 13* | Defense 12* | Magic 4/6* | Toughness 6* | Speed 5*
+## Shot 12
+- **Thunder King**
+- **Brick Manly**
+ 0 Wounds
+ Guns 15 | Defense 14 | Fortune 7/7 | Toughness 7 | Speed 7
+## Shot 10
+- **Ugly Shing**
+- **Jawbuster**
+ 0 Wounds
+ Guns 15 | Defense 14 | Fortune 0/0 | Toughness 7 | Speed 7
+## Shot 9
+- **Hitman**
+## Shot 8
+- **Ninja**
+      TEXT
+    end
+
+    it "shows fight" do
+      response = FightPoster.show(fight)
+      puts
+      puts response
+      puts
+      expect(response).to eq(expected)
+    end
+  end
 end
