e11c7198c13705781dc051f2db84b3a3530eb3eb
Isaac Priestley <isaac.priestley@babylist.com>
2023-06-25 11:42:23 -0400

    Fix spelling of Max Fortune

--- app/services/fight_poster.rb
+++ app/services/fight_poster.rb
@@ -47,7 +47,7 @@ TEXT
 
     def fortune_value(character)
       asterisk = character.impairments.to_i > 0 ? "*" : ""
-      max_fortune = character.action_values["MaxFortune"].to_i - character.impairments.to_i
+      max_fortune = character.action_values["Max Fortune"].to_i - character.impairments.to_i
 
       "#{character.action_values["FortuneType"]} #{character.action_values["Fortune"]}/#{max_fortune}#{asterisk}"
     end
