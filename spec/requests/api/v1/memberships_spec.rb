5cb5d0c49ddf792b1ee9e85ac57ff0358cf54757
Isaac Priestley <isaac.priestley@babylist.com>
2023-07-02 14:52:34 -0400

    Make sure you can belong to multiple parties

--- spec/requests/api/v1/memberships_spec.rb
+++ spec/requests/api/v1/memberships_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "Api::V1::Memberships", type: :request do
   let!(:action_movie) { user.campaigns.create!(title: "Action Movie") }
   let(:brick) { Character.create!(name: "Brick Manly", campaign: action_movie) }
   let!(:party) { Party.create!(name: "The Party", campaign: action_movie) }
+  let!(:other_party) { Party.create!(name: "The Other Party", campaign: action_movie) }
   let(:headers) { Devise::JWT::TestHelpers.auth_headers({}, user) }
 
   before(:each) do
@@ -27,6 +28,16 @@ RSpec.describe "Api::V1::Memberships", type: :request do
       expect {
         post "/api/v1/parties/#{party.id}/memberships", params: { character_id: brick.id }, headers: headers
       }.to change { party.characters.count }.by(1)
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["name"]).to eq("Brick Manly")
+    end
+
+    it "creates a membership to a different party" do
+      party.characters << brick
+      expect {
+        post "/api/v1/parties/#{other_party.id}/memberships", params: { character_id: brick.id }, headers: headers
+      }.to change { other_party.characters.count }.by(1)
       expect(response).to have_http_status(:success)
       body = JSON.parse(response.body)
       expect(body["name"]).to eq("Brick Manly")
