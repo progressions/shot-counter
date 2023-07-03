3ccb415239d3f77da79b73c1441ece93b17f8667
Isaac Priestley <isaac.priestley@babylist.com>
2023-07-03 12:31:09 -0400

    CampaignMembership spec

--- app/models/campaign_membership.rb
+++ app/models/campaign_membership.rb
@@ -1,4 +1,7 @@
 class CampaignMembership < ApplicationRecord
   belongs_to :user
   belongs_to :campaign
+
+  validates :user, presence: true, uniqueness: { scope: :campaign_id }
+  validates :campaign, presence: true
 end
--- spec/models/campaign_membership_spec.rb
+++ spec/models/campaign_membership_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe CampaignMembership, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { User.create!(email: "email@example.com", confirmed_at: Time.now) }
+  let!(:action_movie) { user.campaigns.create!(title: "Action Movie") }
+
+  describe "validations" do
+    it "requires a user" do
+      expect(CampaignMembership.create(user: nil, campaign: action_movie)).to be_invalid
+    end
+
+    it "requires a campaign" do
+      expect(CampaignMembership.create(user: user, campaign: nil)).to be_invalid
+    end
+
+    it "requires a unique user per campaign" do
+      CampaignMembership.create(user: user, campaign: action_movie)
+      expect(CampaignMembership.create(user: user, campaign: action_movie)).to be_invalid
+    end
+  end
 end
