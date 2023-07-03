a6e98f90523e2ab3846b4ea18db2ce82d518d675
Isaac Priestley <isaac.priestley@babylist.com>
2023-07-03 12:12:04 -0400

    Membership spec

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,6 @@
 class Membership < ApplicationRecord
   belongs_to :character
   belongs_to :party
+
+  validates :character_id, uniqueness: { scope: :party_id }
 end
--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Membership, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { User.create!(email: "email@example.com", confirmed_at: Time.now) }
+  let!(:action_movie) { user.campaigns.create!(title: "Action Movie") }
+  let(:brick) { Character.create!(name: "Brick Manly", campaign: action_movie) }
+  let!(:party) { action_movie.parties.create!(name: "Party") }
+
+  describe "validations" do
+    it "validates uniqueness of character_id and party_id to campaign_id" do
+      expect(Membership.create(character: brick, party: party)).to be_valid
+      expect(Membership.create(character: brick, party: party)).to_not be_valid
+    end
+  end
+
+  describe "associations" do
+    it "belongs to a character" do
+      expect(Membership.create(character: brick, party: party).character).to eq(brick)
+    end
+
+    it "belongs to a party" do
+      expect(Membership.create(character: brick, party: party).party).to eq(party)
+    end
+  end
 end
