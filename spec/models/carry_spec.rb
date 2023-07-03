95f3f31ab93a5709c0c38daa65a66d9f330ea048
Isaac Priestley <isaac.priestley@babylist.com>
2023-07-03 12:23:43 -0400

    Carry spec

--- spec/models/carry_spec.rb
+++ spec/models/carry_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Carry, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { User.create!(email: "email@example.com", confirmed_at: Time.now) }
+  let!(:action_movie) { user.campaigns.create!(title: "Action Movie") }
+  let(:brick) { Character.create!(name: "Brick Manly", campaign: action_movie) }
+  let(:weapon) { Weapon.create!(name: "Sword", damage: "10", campaign: action_movie) }
+
+  describe "validations" do
+    it "allows multiple carries of the same weapon" do
+      expect(Carry.create(character: brick, weapon: weapon)).to be_valid
+      expect(Carry.create(character: brick, weapon: weapon)).to be_valid
+    end
+  end
+
+  describe "associations" do
+    it "belongs to a character" do
+      expect(Carry.create(character: brick, weapon: weapon).character).to eq(brick)
+    end
+
+    it "belongs to a weapon" do
+      expect(Carry.create(character: brick, weapon: weapon).weapon).to eq(weapon)
+    end
+  end
 end
