5ddff1c7f1093f8b6cebf3bfd7250886adb80457
Isaac Priestley <isaac.priestley@babylist.com>
2023-07-03 12:33:23 -0400

    Attunement spec

--- app/models/attunement.rb
+++ app/models/attunement.rb
@@ -1,4 +1,7 @@
 class Attunement < ApplicationRecord
   belongs_to :character
   belongs_to :site
+
+  validates :character, presence: true, uniqueness: { scope: :site_id }
+  validates :site, presence: true
 end
--- spec/models/attunement_spec.rb
+++ spec/models/attunement_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Attunement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { User.create!(email: "email@example.com", confirmed_at: Time.now) }
+  let!(:action_movie) { user.campaigns.create!(title: "Action Movie") }
+  let(:brick) { Character.create!(name: "Brick Manly", campaign: action_movie) }
+  let(:site) { Site.create!(name: "Site", campaign: action_movie) }
+
+  describe "validations" do
+    it "validates uniqueness of character to site" do
+      expect(Attunement.create(character: brick, site: site)).to be_valid
+      expect(Attunement.create(character: brick, site: site)).not_to be_valid
+    end
+  end
+
+  describe "associations" do
+    it "belongs to a character" do
+      expect(Attunement.create(character: brick, site: site).character).to eq(brick)
+    end
+
+    it "belongs to a site" do
+      expect(Attunement.create(character: brick, site: site).site).to eq(site)
+    end
+  end
 end
