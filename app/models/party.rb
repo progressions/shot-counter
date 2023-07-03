907b0950e8f747f5afb1fdc36095007fcfb8e489
Isaac Priestley <isaac.priestley@babylist.com>
2023-07-02 20:45:40 -0400

    Destroy memberships when destroying site

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,5 +1,5 @@
 class Party < ApplicationRecord
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :characters, through: :memberships
   belongs_to :campaign
 end
--- app/models/site.rb
+++ app/models/site.rb
@@ -1,7 +1,7 @@
 class Site < ApplicationRecord
   belongs_to :campaign
   belongs_to :faction, optional: true
-  has_many :attunements
+  has_many :attunements, dependent: :destroy
   has_many :characters, through: :attunements
 
   validates :name, presence: true, uniqueness: { scope: :campaign_id }
