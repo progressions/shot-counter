bd2d4b2f531a2e3bd064ecee9ded766ed94ff118
Isaac Priestley <isaac.priestley@babylist.com>
2023-07-03 12:39:38 -0400

    User mailer spec

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,16 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "password_reset" do
+    let(:user) { User.create!(email: "email@example.com", password: "password", password_confirmation: "password") }
+    let(:action_movie) { user.campaigns.create!(title: "Action Movie") }
+    let(:invitation) { Invitation.create!(email: "someone@example.com", user: user, campaign: action_movie) }
+    let(:mail) { UserMailer.with(invitation: invitation).invitation }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("You have been invited to join Action Movie in the Chi War!")
+      expect(mail.to).to eq(["someone@example.com"])
+      expect(mail.from).to eq(["isaac@chiwar.net"])
+    end
+  end
 end
