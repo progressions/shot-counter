094ab92beca3842d9e38cfd95365c0dc6347ff9f
Isaac Priestley <isaac.priestley@babylist.com>
2023-07-03 12:01:36 -0400

    Site spec

--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Site, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { User.create!(email: "email@example.com", confirmed_at: Time.now) }
+  let!(:action_movie) { user.campaigns.create!(title: "Action Movie") }
+  let(:brick) { Character.create!(name: "Brick Manly", campaign: action_movie) }
+
+  describe "validations" do
+    it "has a name" do
+      site = Site.create!(name: "The Cave", campaign: action_movie)
+      expect(site.name).to eq("The Cave")
+    end
+
+    it "has a campaign" do
+      site = Site.create!(name: "The Cave", campaign: action_movie)
+      expect(site.campaign).to eq(action_movie)
+    end
+
+    it "can have characters" do
+      site = Site.create!(name: "The Cave", campaign: action_movie)
+      site.characters << brick
+      expect(site.characters).to include(brick)
+    end
+  end
+
+  describe "associations" do
+    it "belongs to a campaign" do
+      site = Site.create!(name: "The Cave", campaign: action_movie)
+      expect(site.campaign).to eq(action_movie)
+    end
+
+    it "can have characters" do
+      site = Site.create!(name: "The Cave", campaign: action_movie)
+      site.characters << brick
+      expect(site.characters).to include(brick)
+    end
+  end
 end
