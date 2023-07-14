8dcf91f22c8951a7eea6b96e2cb4652d185ed897
Isaac Priestley <isaac.priestley@babylist.com>
2023-07-13 20:40:25 -0400

    It seems to work so far

--- app/controllers/api/v1/actors_controller.rb
+++ app/controllers/api/v1/actors_controller.rb
@@ -39,6 +39,9 @@ class Api::V1::ActorsController < ApplicationController
   def create
     @character = current_campaign.characters.create!(character_params.merge(user: current_user))
     @shot = @fight.shots.build(character_id: @character.id, shot: shot_params[:current_shot])
+    if @character.action_values["Type"] == "Mook"
+      @shot.mook = Mook.new(count: @character.action_values["Wounds"], color: character_params[:color])
+    end
 
     if @shot.save
       render json: @character
@@ -50,7 +53,17 @@ class Api::V1::ActorsController < ApplicationController
   def update
     current_shot = shot_params[:current_shot] == "hidden" ? nil : shot_params[:current_shot]
     @shot.update(shot: current_shot) if shot_params[:current_shot]
-    if @character.update(character_params)
+
+    parms = character_params
+
+    if @character.action_values["Type"] == "Mook"
+      count = params[:character][:count]
+      @shot.mook ||= Mook.new(count: @character.action_values["Wounds"], color: character_params[:color])
+      @shot.mook.update(count: count, color: character_params[:color])
+      parms = mook_params
+    end
+
+    if @character.update(parms)
       render json: @character
     else
       render @character.errors, status: 400
@@ -98,6 +111,16 @@ class Api::V1::ActorsController < ApplicationController
       .permit(:name, :defense, :impairments, :color,
               :user_id, :active, skills: [],
               action_values: Character::DEFAULT_ACTION_VALUES.keys,
+              description: Character::DEFAULT_DESCRIPTION.keys,
+              schticks: [])
+  end
+
+  def mook_params
+    params
+      .require(:character)
+      .permit(:name, :defense, :impairments, :color,
+              :user_id, :active, skills: [],
+              action_values: Character::DEFAULT_ACTION_VALUES.keys - ["Wounds"],
               description: Character::DEFAULT_DESCRIPTION.keys,
               schticks: [])
   end
