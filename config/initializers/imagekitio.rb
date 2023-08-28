d9018a1cf33d223dc79c59ebfd4d2cbc9b62e390
Isaac Priestley <isaac.priestley@babylist.com>
2023-08-28 11:27:18 -0500

    Not just development

--- config/initializers/imagekitio.rb
+++ config/initializers/imagekitio.rb
@@ -1,10 +1,7 @@
 ImageKitIo.configure do |config|
-  if Rails.env.development?
-    config.public_key = Rails.application.credentials.imagekit&.fetch(:public_key, "")
-    config.private_key = Rails.application.credentials.imagekit&.fetch(:private_key, "")
-    config.url_endpoint = "https://ik.imagekit.io/nvqgwnjgv/chi-war-#{Rails.env}/"
-  end
+  config.public_key = Rails.application.credentials.imagekit&.fetch(:public_key, "")
+  config.private_key = Rails.application.credentials.imagekit&.fetch(:private_key, "")
+  config.url_endpoint = "https://ik.imagekit.io/nvqgwnjgv/chi-war-#{Rails.env}/"
   config.service = :active_storage
   # config.constants.MISSING_PRIVATE_KEY = 'custom error message'
 end
-#make sure to replace the your_public_key, your_private_key and your_url_endpoint with actual values
