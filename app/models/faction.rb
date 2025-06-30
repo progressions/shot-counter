3e0f97ab158680a6748d432e83509c44ed8b7e34
Isaac Priestley <isaac.priestley@babylist.com>
2025-06-29 23:41:01 -0400

    Faction json payload

--- app/models/faction.rb
+++ app/models/faction.rb
@@ -7,4 +7,23 @@ class Faction < ApplicationRecord
   has_one_attached :image
 
   validates :name, presence: true, uniqueness: { scope: :campaign_id }
+
+  def as_json(args = {})
+    {
+      id: id,
+      name: name,
+      description: description,
+      active: self.active,
+      created_at: created_at,
+      updated_at: updated_at,
+      characters: characters.map { |character|
+        {
+          id: character.id,
+          name: character.name,
+          image_url: character.image_url,
+        }
+      },
+      image_url: image.attached? ? image.url : nil,
+    }
+  end
 end
