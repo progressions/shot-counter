81075412b7252bebd18babfbc05dee088d698153
Isaac Priestley <progressions@gmail.com>
2025-08-05 16:58:58 -0400

    Show image_url

--- app/serializers/encounter_serializer.rb
+++ app/serializers/encounter_serializer.rb
@@ -1,11 +1,15 @@
 class EncounterSerializer < ActiveModel::Serializer
-  attributes :id, :entity_class, :name, :sequence, :description, :shots
+  attributes :id, :entity_class, :name, :sequence, :description, :shots, :started_at, :ended_at, :image_url
 
   def entity_class
     "Encounter"
   end
 
   def shots
+    # Load characters to call image_url
+    character_ids = object.shots.pluck(:character_id).uniq.compact
+    characters_by_id = Character.where(id: character_ids).index_by(&:id)
+
     object.shots
       .where(fight_id: object.id)
       .joins("LEFT JOIN characters ON characters.id = shots.character_id")
@@ -31,7 +35,15 @@ class EncounterSerializer < ActiveModel::Serializer
         ) FILTER (WHERE characters.id IS NOT NULL) AS characters"
       )
       .map do |record|
-        [record.shot, record.characters || []]
+        characters = (record.characters || []).map do |character|
+          character_model = characters_by_id[character["id"]]
+          character.merge("image_url" => character_model&.image_url)
+        end
+        [record.shot, characters]
       end
+  end
+
+  def image_url
+    object.image.attached? ? object.image.url : nil
   end
 end
