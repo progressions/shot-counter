efa1d5ba03539de5292a901bece357fb400dfef1
Isaac Priestley <progressions@gmail.com>
2025-08-07 20:03:45 -0400

    optimize some queries

---
+++ app/serializers/campaign_lite_serializer.rb
@@ -0,0 +1,16 @@
+class CampaignLiteSerializer < ActiveModel::Serializer
+  attributes :id, :name, :description, :gamemaster_id, :gamemaster, :created_at, :updated_at, :players, :player_ids, :image_url, :entity_class
+  has_many :image_positions, serializer: ImagePositionSerializer
+
+  def gamemaster
+    UserSerializer.new(object.user, scope: scope, root: false)
+  end
+
+  def gamemaster_id
+    object.user_id
+  end
+
+  def entity_class
+    object.class.name
+  end
+end
