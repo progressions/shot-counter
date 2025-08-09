8ba216e6380cbc5c3bcfd95a85ec97650a142d67
Isaac Priestley <progressions@gmail.com>
2025-08-09 14:03:44 -0400

    Spec for autocomplete

--- spec/requests/api/v2/characters_spec.rb
+++ spec/requests/api/v2/characters_spec.rb
@@ -29,6 +29,7 @@ RSpec.describe "Api::V2::Characters", type: :request do
     @featured_foe = Character.create!(name: "Amanda Yin", action_values: { "Type" => "Featured Foe" }, campaign_id: @campaign.id, faction_id: @ascended.id, user_id: @gamemaster.id)
     @mook = Character.create!(name: "Thug", action_values: { "Type" => "Mook" }, campaign_id: @campaign.id, faction_id: @ascended.id, user_id: @gamemaster.id)
     @ally = Character.create!(name: "Angie Lo", action_values: { "Type" => "Ally" }, campaign_id: @campaign.id, faction_id: @dragons.id, user_id: @gamemaster.id)
+    @dead_guy = Character.create!(name: "Dead Guy", action_values: { "Type" => "PC", "Archetype" => "Everyday Hero" }, campaign_id: @campaign.id, faction_id: @dragons.id, user_id: @gamemaster.id, active: false)
 
     @headers = Devise::JWT::TestHelpers.auth_headers({}, @gamemaster)
     set_current_campaign(@gamemaster, @campaign)
@@ -50,7 +51,6 @@ RSpec.describe "Api::V2::Characters", type: :request do
       expect(response).to have_http_status(:success)
       body = JSON.parse(response.body)
       expect(body["characters"].length).to eq(1)
-      puts body["characters"][0].inspect
       expect(body["characters"][0]).to include("name" => "Brick Manly", "faction_id" => @dragons.id, "entity_class" => "Character")
       expect(body["characters"][0].keys).to eq(["id", "name", "image_url", "faction_id", "action_values", "created_at", "updated_at", "description", "entity_class", "skills", "schticks", "image_positions"])
     end
@@ -274,5 +274,235 @@ RSpec.describe "Api::V2::Characters", type: :request do
   end
 
   describe "GET /autocomplete" do
+    it "gets all characters" do
+      get "/api/v2/characters/names", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["characters"].map { |c| c["name"] }).to eq(["Angie Lo", "Thug", "Amanda Yin", "Ugly Shing", "Serena", "Brick Manly"])
+      expect(body["factions"].map { |f| f["name"] }).to eq(["The Ascended", "The Dragons"])
+    end
+
+    it "returns character attributes" do
+      get "/api/v2/characters/names?search=Brick", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["characters"].length).to eq(1)
+      expect(body["characters"][0]).to include("name" => "Brick Manly")
+      expect(body["characters"][0].keys).to eq(["id", "name"])
+    end
+
+    it "returns an empty array when no characters exist" do
+      Character.delete_all
+      get "/api/v2/characters/names", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["characters"]).to eq([])
+      expect(body["factions"]).to eq([])
+    end
+
+    it "sorts by created_at ascending" do
+      get "/api/v2/characters/names?sort=created_at&order=asc", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["characters"].map { |c| c["name"] }).to eq(["Brick Manly", "Serena", "Ugly Shing", "Amanda Yin", "Thug", "Angie Lo"])
+      expect(body["factions"].map { |f| f["name"] }).to eq(["The Ascended", "The Dragons"])
+    end
+
+    it "sorts by created_at descending" do
+      get "/api/v2/characters/names?sort=created_at&order=desc", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["characters"].map { |c| c["name"] }).to eq(["Angie Lo", "Thug", "Amanda Yin", "Ugly Shing", "Serena", "Brick Manly"])
+      expect(body["factions"].map { |f| f["name"] }).to eq(["The Ascended", "The Dragons"])
+    end
+
+    it "sorts by updated_at ascending" do
+      @brick.touch
+      get "/api/v2/characters/names?sort=updated_at&order=asc", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["characters"].map { |c| c["name"] }).to eq(["Serena", "Ugly Shing", "Amanda Yin", "Thug", "Angie Lo", "Brick Manly"])
+      expect(body["factions"].map { |f| f["name"] }).to eq(["The Ascended", "The Dragons"])
+    end
+
+    it "sorts by updated_at descending" do
+      @brick.touch
+      get "/api/v2/characters/names?sort=updated_at&order=desc", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["characters"].map { |c| c["name"] }).to eq(["Brick Manly", "Angie Lo", "Thug", "Amanda Yin", "Ugly Shing", "Serena"])
+      expect(body["factions"].map { |f| f["name"] }).to eq(["The Ascended", "The Dragons"])
+    end
+
+    it "sorts by name ascending" do
+      get "/api/v2/characters/names?sort=name&order=asc", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["characters"].map { |c| c["name"] }).to eq(["Amanda Yin", "Angie Lo", "Brick Manly", "Serena", "Thug", "Ugly Shing"])
+      expect(body["factions"].map { |f| f["name"] }).to eq(["The Ascended", "The Dragons"])
+    end
+
+    it "sorts by name descending" do
+      get "/api/v2/characters/names?sort=name&order=desc", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["characters"].map { |c| c["name"] }).to eq(["Ugly Shing", "Thug", "Serena", "Brick Manly", "Angie Lo", "Amanda Yin"])
+      expect(body["factions"].map { |f| f["name"] }).to eq(["The Ascended", "The Dragons"])
+    end
+
+    it "sorts by type ascending" do
+      get "/api/v2/characters/names?sort=type&order=asc", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["characters"].map { |c| c["name"] }).to eq(["Angie Lo", "Ugly Shing", "Amanda Yin", "Thug", "Brick Manly", "Serena"])
+      expect(body["characters"].map { |c| c["action_values"] }.compact).to eq([])
+      expect(body["factions"].map { |f| f["name"] }).to eq(["The Ascended", "The Dragons"])
+    end
+
+    it "sorts by type descending" do
+      get "/api/v2/characters/names?sort=type&order=desc", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["characters"].map { |c| c["name"] }).to eq(["Brick Manly", "Serena", "Thug", "Amanda Yin", "Ugly Shing", "Angie Lo"])
+      expect(body["factions"].map { |f| f["name"] }).to eq(["The Ascended", "The Dragons"])
+    end
+
+    it "sorts by archetype ascending" do
+      get "/api/v2/characters/names?sort=archetype&order=asc", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["characters"].map { |c| c["name"] }).to eq(["Amanda Yin", "Angie Lo", "Thug", "Ugly Shing", "Brick Manly", "Serena"])
+      expect(body["factions"].map { |f| f["name"] }).to eq(["The Ascended", "The Dragons"])
+    end
+
+    it "sorts by archetype descending" do
+      get "/api/v2/characters/names?sort=archetype&order=desc", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["characters"].map { |c| c["name"] }).to eq(["Serena", "Brick Manly", "Amanda Yin", "Angie Lo", "Thug", "Ugly Shing"])
+      expect(body["factions"].map { |f| f["name"] }).to eq(["The Ascended", "The Dragons"])
+    end
+
+    it "filters by faction_id" do
+      get "/api/v2/characters/names?faction_id=#{@dragons.id}", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["characters"].map { |c| c["name"] }).to eq(["Angie Lo", "Serena", "Brick Manly"])
+      expect(body["factions"].map { |f| f["name"] }).to eq(["The Dragons"])
+    end
+
+    it "filters by user_id" do
+      get "/api/v2/characters/names?user_id=#{@gamemaster.id}", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["characters"].map { |c| c["name"] }).to eq(["Angie Lo", "Thug", "Amanda Yin", "Ugly Shing"])
+      expect(body["factions"].map { |f| f["name"] }).to eq(["The Ascended", "The Dragons"])
+    end
+
+    it "filters by search string" do
+      get "/api/v2/characters/names?search=Ugly", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["characters"].map { |c| c["name"] }).to eq(["Ugly Shing"])
+      expect(body["factions"].map { |f| f["name"] }).to eq(["The Ascended"])
+    end
+
+    it "filters by search string" do
+      get "/api/v2/characters/names?search=Brick", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["characters"].map { |c| c["name"] }).to eq(["Brick Manly"])
+      expect(body["factions"].map { |f| f["name"] }).to eq(["The Dragons"])
+    end
+
+    it "filters by character type" do
+      get "/api/v2/characters/names?type=Boss", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["characters"].map { |c| c["name"] }).to eq(["Ugly Shing"])
+      expect(body["factions"].map { |f| f["name"] }).to eq(["The Ascended"])
+    end
+
+    it "filters by character type" do
+      get "/api/v2/characters/names?type=PC", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["characters"].map { |c| c["name"] }).to eq(["Serena", "Brick Manly"])
+      expect(body["factions"].map { |f| f["name"] }).to eq(["The Dragons"])
+    end
+
+    it "filters by archetype" do
+      get "/api/v2/characters/names?archetype=Sorcerer", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["characters"].map { |c| c["name"] }).to eq(["Serena"])
+      expect(body["factions"].map { |f| f["name"] }).to eq(["The Dragons"])
+    end
+
+    it "filters by archetype" do
+      get "/api/v2/characters/names?archetype=Everyday%20Hero", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["characters"].map { |c| c["name"] }).to eq(["Brick Manly"])
+      expect(body["factions"].map { |f| f["name"] }).to eq(["The Dragons"])
+    end
+
+    it "filters by party" do
+      @dragons_party.characters << @brick
+      get "/api/v2/characters/names?party_id=#{@dragons_party.id}", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["characters"].map { |c| c["name"] }).to eq(["Brick Manly"])
+      expect(body["factions"].map { |f| f["name"] }).to eq(["The Dragons"])
+    end
+
+    it "filters by party" do
+      @ascended_party.characters << @boss
+      @ascended_party.characters << @mook
+      get "/api/v2/characters/names?party_id=#{@ascended_party.id}", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["characters"].map { |c| c["name"] }).to eq(["Thug", "Ugly Shing"])
+      expect(body["factions"].map { |f| f["name"] }).to eq(["The Ascended"])
+    end
+
+    it "filters by fight" do
+      @fight.characters << @brick
+      @fight.characters << @serena
+      @fight.characters << @boss
+      get "/api/v2/characters/names?sort=name&order=asc&fight_id=#{@fight.id}", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["characters"].map { |c| c["name"] }).to eq(["Brick Manly", "Serena", "Ugly Shing"])
+      expect(body["factions"].map { |f| f["name"] }).to eq(["The Ascended", "The Dragons"])
+    end
+
+    it "filters by site" do
+      @dragons_hq.characters << @brick
+      @dragons_hq.characters << @serena
+      get "/api/v2/characters/names?site_id=#{@dragons_hq.id}", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["characters"].map { |c| c["name"] }).to eq(["Serena", "Brick Manly"])
+      expect(body["factions"].map { |f| f["name"] }).to eq(["The Dragons"])
+    end
+
+    it "filters by site" do
+      @ascended_hq.characters << @boss
+      @ascended_hq.characters << @featured_foe
+      get "/api/v2/characters/names?site_id=#{@ascended_hq.id}", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["characters"].map { |c| c["name"] }).to eq(["Amanda Yin", "Ugly Shing"])
+      expect(body["factions"].map { |f| f["name"] }).to eq(["The Ascended"])
+    end
+
+    it "filters by is_template" do
+      get "/api/v2/characters/names?is_template=true", headers: @headers
+      expect(response).to have_http_status(:success)
+      body = JSON.parse(response.body)
+      expect(body["characters"].map { |c| c["name"] }).to eq(["Bandit"])
+      expect(body["factions"].map { |f| f["name"] }).to eq([])
+    end
   end
 end
