cf8d1efa33fe2bec3a9ed893c5d5c058100fba86
Isaac Priestley <progressions@gmail.com>
2025-08-29 17:00:19 -0400

    Update imagekit id

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
-  config.action_cable.allowed_request_origins = [ "https://chiwar.net"]
+  config.action_cable.allowed_request_origins = [ "https://chiwar.net", "https://shot-client-next.fly.dev"]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
