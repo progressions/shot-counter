554ef84c72b00e82730b71c06a4c56d274763982
Isaac Priestley <progressions@gmail.com>
2025-08-30 17:17:34 -0400

    Remove debug endpoints and test job

--- app/jobs/test_job.rb
+++
@@ -1,8 +0,0 @@
-class TestJob < ApplicationJob
-  queue_as :default
-
-  def perform(message = "Test")
-    Rails.logger.info "[TestJob] EXECUTING - Message: #{message} at #{Time.current}"
-    puts "[TestJob] EXECUTING - Message: #{message} at #{Time.current}"
-  end
-end
