84d82d56d8b1773cd6f399df8137439902a75db5
Isaac Priestley <git@progressions.me>
2025-11-08 16:28:12 -0500

    Copy image by category

--- app/services/campaign_image_copy_service.rb
+++ app/services/campaign_image_copy_service.rb
@@ -1,29 +1,37 @@
 module CampaignImageCopyService
   class << self
     def call(target_campaign:, source_campaign: nil, importer: ImageKitImporter, force: false, logger: Rails.logger)
-      source_campaign.schticks.each do |a|
-        puts "Copying image from #{a.name}..."
+      # collect schtick category images
+      schtick_images_by_category = source_campaign.schticks
+        .where.not(category: nil)
+        .group_by(&:category)
+        .transform_values { |schticks| schticks.find { |w| w.image_url.present? }&.image_url }
+        .compact
+
+      target_campaign.schticks.each do |b|
         begin
-          b = target_campaign.schticks.find_by(name: a.name)
-          puts "...into #{b.name}"
+          image_for_category = schtick_images_by_category[b.category]
 
-          next unless a.image_url
-
-          ImageKitImporter.call(source_url: a.image_url, attachable: b)
+          ImageKitImporter.call(source_url: image_for_category, attachable: b)
         rescue StandardError => e
           puts e.inspect
         end
       end
 
-      source_campaign.weapons.each do |a|
-        puts "Copying image from #{a.name}..."
+
+      # collect weapon category images
+      weapon_images_by_category = source_campaign.weapons
+        .where.not(category: nil)
+        .group_by(&:category)
+        .transform_values { |weapons| weapons.find { |w| w.image_url.present? }&.image_url }
+        .compact
+
+      target_campaign.weapons.each do |b|
         begin
-          b = target_campaign.weapons.find_by(name: a.name)
-          puts "...into #{b.name}"
+          image_for_category = weapon_images_by_category[b.category]
 
-          next unless a.image_url
+          ImageKitImporter.call(source_url: image_for_category, attachable: b)
 
-          ImageKitImporter.call(source_url: a.image_url, attachable: b)
         rescue StandardError => e
           puts e.inspect
         end
