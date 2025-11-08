9423b4c3b9f69fb19b60cf1741fd06c33f25a70e
Isaac Priestley <git@progressions.me>
2025-11-08 10:49:02 -0500

    Try fixing image url

--- app/models/concerns/with_imagekit.rb
+++ app/models/concerns/with_imagekit.rb
@@ -13,12 +13,26 @@ module WithImagekit
     cache_key = "image_url/#{self.class.name}/#{id}/#{image.attachment.id}"
     Rails.cache.fetch(cache_key, expires_in: 1.hour) do
       metadata = image.blob.metadata || {}
-      legacy_url = metadata["url"].presence || metadata[:url].presence
+      legacy_url = [
+        metadata["imagekit_url"],
+        metadata[:imagekit_url],
+        metadata["url"],
+        metadata[:url]
+      ].compact_blank.first
       next legacy_url if legacy_url.present?
+
+      path_source =
+        metadata["imagekit_file_path"].presence ||
+        metadata[:imagekit_file_path].presence ||
+        image.blob.key
+
+      path_url = build_imagekit_url_for(path_source)
+      next path_url if path_url.present?
 
       begin
         image.blob.service_url
-      rescue NotImplementedError, NoMethodError
+      rescue NotImplementedError, NoMethodError => e
+        Rails.logger.debug("ImageKit service_url unavailable for #{self.class.name}##{id}: #{e.message}") if defined?(Rails)
         blob_url_fallback
       end
     rescue StandardError => e
@@ -32,6 +46,13 @@ module WithImagekit
   end
 
   private
+
+  def build_imagekit_url_for(path)
+    endpoint = Imagekit::Rails.configuration.url_endpoint if defined?(Imagekit::Rails)
+    return nil if endpoint.blank? || path.blank?
+
+    "#{endpoint.chomp('/')}/#{path.to_s.delete_prefix('/')}"
+  end
 
   def blob_url_fallback
     configured_options = Rails.configuration.x.try(:active_storage_url_options)
